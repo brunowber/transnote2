359aa9cc471893fcd1fdaaa22588988507948ed7
Michel Nassib Fonseca <michel.fonseca@joinville.sc.gov.br>
2017-08-09 11:24:21 -0300

    Correção local banco.py

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,6 +6,6 @@ sed -i "s/db_name/$DB_NAME/g
 s/user_db/$USER_DB/g
 s/password_db/$PASSWORD_DB/g
 s/host_db/$HOST_DB/g
-s/port_db/$PORT_DB/g" /usr/local/apache2/htdocs/config.xml
+s/port_db/$PORT_DB/g" /talonario/detrans/settings/banco.py
 
 python manage.py runserver 0.0.0.0:8000
