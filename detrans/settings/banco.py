5695a368fabb56dfb0af48fe2a1d63241078a6ec
Michel Nassib Fonseca <michel.fonseca@joinville.sc.gov.br>
2017-08-09 11:09:00 -0300

    Correção database name.

--- detrans/settings/banco.py
+++ detrans/settings/banco.py
@@ -5,7 +5,7 @@ from detrans.settings.base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db',
+        'NAME': 'db_name',
         'USER': 'user_db',
         'PASSWORD': 'password_db',
         'HOST': 'host_db',
