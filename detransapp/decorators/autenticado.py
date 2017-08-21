60263212d0fba2e67ad4e5c933a5b11a29ede928
webeer <brunowber@gmail.com>
2017-08-21 14:43:13 -0300

    Decorator autenticado criado

---
+++ detransapp/decorators/autenticado.py
@@ -0,0 +1,18 @@
+from django.shortcuts import redirect
+
+
+def autenticado():
+    def _dec(view_func):
+        def _view(request, *args, **kwargs):
+            print (request.user)
+            if str(request.user) == "AnonymousUser":
+                print "403 - Sem permissao de acesso!"
+                return redirect('/')
+            return view_func(request, *args, **kwargs)
+
+        _view.__name__ = view_func.__name__
+        _view.__dict__ = view_func.__dict__
+        _view.__doc__ = view_func.__doc__
+
+        return _view
+    return _dec
