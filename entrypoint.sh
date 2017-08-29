18e803d2383ad68a4f47aba01b698311cfdb5f2a
Bruno Luan Weber <u89349@pmjlle.joinville.sc.gov.br>
2017-08-29 13:20:30 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,5 @@ s/password_db/$PASSWORD_DB/g
 s/host_db/$HOST_DB/g
 s/port_db/$PORT_DB/g" ./detrans/settings/banco.py
 
-python manage.py makemigrations
 python manage.py migrate
 python manage.py runserver 0.0.0.0:8000
