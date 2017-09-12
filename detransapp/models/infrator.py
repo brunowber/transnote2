b9a96ba2d2e2a3dab21753dfb44d0df421f701bb
Bruno Luan Weber <u89349@pmjlle.joinville.sc.gov.br>
2017-09-12 15:46:08 -0300

    Retorno dos dados do infrator

--- detransapp/models/infrator.py
+++ detransapp/models/infrator.py
@@ -7,9 +7,9 @@ from detransapp.models.pessoa import Pessoa
 class Infrator(Pessoa):
     """Classe para models de infratores"""
 
-    #estado = models.CharField(max_length=255, null=True)
-    #cidade = models.CharField(max_length=255, null=True)
-    #endereco = models.CharField(max_length=255, null=True)
+    estado = models.CharField(max_length=255, null=True)
+    cidade = models.CharField(max_length=255, null=True)
+    endereco = models.CharField(max_length=255, null=True)
 
     def __unicode__(self):
         return self.nome
