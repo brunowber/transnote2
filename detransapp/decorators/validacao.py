9b8a90ca52ff44d545260dc78fcbb9f688941ab2
weber <brunowber@gmail.com>
2017-12-05 12:00:37 -0200

    Correção validação na carga inicial.

--- detransapp/decorators/validacao.py
+++ detransapp/decorators/validacao.py
@@ -8,20 +8,18 @@ from detransapp.rest import JSONResponse
 def validar_imei():
     def _dec(view_func):
         def _view(request, *args, **kwargs):
-            
             if 'imei' in request.POST:
-                print request.POST['imei']
-                print
                 dispositivo = Dispositivo.objects.existe_dispositivo(request.POST['imei'])
                 if dispositivo:
                     return view_func(request, *args, **kwargs)
                 else:
                     acesso = Acesso.objects.get_or_create(imei=request.POST['imei'])[0]
-                    acesso.usuario = Agente.objects.get(id=request.POST['agente']).username
+                    try:
+                        acesso.usuario = Agente.objects.get(id=request.POST['agente']).username
+                    except:
+                        acesso.usuario = Agente.objects.get(username=request.POST['username']).username
                     acesso.dt_acesso = datetime.datetime.now()
-                    print "datetime", acesso.dt_acesso
                     acesso.save()
-
             return JSONResponse({'status': 'Dispositivo nao encontrado!'},
                                 status=status.HTTP_403_FORBIDDEN)
 
