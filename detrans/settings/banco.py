6fa088efbbc46f2233a34b5a385c07041b6796fc
weber <brunowber@gmail.com>
2018-03-07 14:04:08 -0300

    Correção BD. Criação local de acesso sem permissão. Correção criar agente.

--- detrans/settings/banco.py
+++ detrans/settings/banco.py
@@ -5,10 +5,15 @@ from detrans.settings.base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'db_name',
-        'USER': 'user_db',
-        'PASSWORD': 'password_db',
-        'HOST': 'host_db',
-        'PORT': 'port_db',
+        # 'NAME': 'db_name',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        # 'USER': 'user_db',
+        'PASSWORD': 'postgres',
+        # 'PASSWORD': 'password_db',
+        'HOST': '191.52.62.38',
+        # 'HOST': 'host_db',
+        'PORT': '5432',
+        # 'PORT': 'port_db',
     }
 }
